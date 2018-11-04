32b607843732cc7f983190451e68bcfd335d762f
Grzegorz Szermiński <szerman@poczta.fm>
2018-11-04 08:49:33 +0100

    Completed lodash-sort me

--- lodash/sortMe.js
+++ lodash/sortMe.js
@@ -1,12 +1,6 @@
 const lodash = require("lodash");
 
-const articlesList = [
-	{ article: 41,   quantity: 24 },
-    { article: 2323, quantity: 2  },
-    { article: 655,  quantity: 23 }
-];
-
-const worker = function() {
+const worker = function(articlesList) {
        return lodash.sortBy(articlesList, 'quantity').reverse();
     };
     // export the worker function as a nodejs module
