da50e80b94106067f6dbcfa9fc1cf88188d3727d
Grzegorz Szermiński <szerman@poczta.fm>
2018-11-05 11:30:15 +0100

    Completed lololodash-in every case

---
+++ lodash/InEveryCase.js
@@ -0,0 +1,15 @@
+ const lodash = require("lodash");
+
+    const worker = function(city) {
+        return lodash.forEach(city, function(item) {
+        	if (item.population > 1) {
+        		item.size = 'big';
+        	} else if (item.population > 0.5) {
+        		item.size = 'med';
+        	} else {
+        		item.size = 'small';
+        	}
+        });
+    };
+
+module.exports = worker;
