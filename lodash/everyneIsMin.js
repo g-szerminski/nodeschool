88d6faf6a2e18e77019dc1cacce07cf383aa60a2
Grzegorz Szermiński <szerman@poczta.fm>
2018-11-06 22:48:20 +0100

    Work In Progress

--- lodash/everyneIsMin.js
+++
@@ -1,9 +0,0 @@
-const = require('lodash');
-
-const worker = function(city) {
-	if (lodash.every(city, function(avgTemp) > 19 {
-			return city.push('hot');
-	}));
-};
-
-module.exports = worker;
--- lodash/everyoneIsMin.js
+++ lodash/everyoneIsMin.js
@@ -1,9 +1,52 @@
-const = require("lodash");
+const lodash = require("lodash");
 
-const worker = function(city) {
-	if (lodash.every(city, function(avgTemp) > 19 {
-			return city.push('hot');
-	}));
+var townList = function (item) {
+	var rusult = {
+		hot = [],
+		warm = []
+	};
+
+	const checkTemp = function (item) {
+		item > 19;
+	};
+	// var checkTemp = (item) => item > 19;
+
+	lodash.forEach(item, function (town, townName) {
+		if (lodash.every(town, checkTemp)) {
+			result.hot.push(townName);
+		} else if (lodash.some(town, checkTemp)) {
+			result.warm.push(townName);
+		}
+	});
+	return result;
 };
 
-module.exports = worker;
+module.exports = townList;
+
+
+
+// const _ = require("lodash");
+
+// var tempsort = function (item) {
+    
+//     var result = {
+//         hot: [],
+//         warm: []
+//     };
+    
+//     // If temp > 19
+//     const check_temp = (item) => item > 19;
+
+//     _.forEach(item, function (town, townname) {
+
+//         if (_.every(town, check_temp)) {
+//             result.hot.push(townname);
+//         } else if (_.some(town, check_temp)) {
+//             result.warm.push(townname);
+//         }
+
+//     });
+
+//     return result;
+// };
+// module.exports = tempsort;
