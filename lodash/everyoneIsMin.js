6d1fd8e7a0c2b90d99216a751fb6f73ed1a793d3
Grzegorz Szermiński <szerman@poczta.fm>
2018-11-07 20:38:47 +0100

    Completed lololodash- every one is min

--- lodash/everyoneIsMin.js
+++ lodash/everyoneIsMin.js
@@ -1,15 +1,16 @@
 const lodash = require('lodash');
 
-var townList = function (item) {
-	var rusult = {
+const townList = function (item) {
+	var result = {
 		hot: [],
 		warm: []
 	};
 
 	const checkTemp = function (item) {
-		item > 19;
+		const temp = item > 19;
+		return temp;
 	};
-	// var checkTemp = (item) => item > 19;
+	// const checkTemp = (item) => item > 19;
 
 	lodash.forEach(item, function (town, townName) {
 		if (lodash.every(town, checkTemp)) {
@@ -22,31 +23,3 @@ var townList = function (item) {
 };
 
 module.exports = townList;
-
-
-
-// const _ = require("lodash");
-
-// var tempsort = function (item) {
-    
-//     var result = {
-//         hot: [],
-//         warm: []
-//     };
-    
-//     // If temp > 19
-//     const check_temp = (item) => item > 19;
-
-//     _.forEach(item, function (town, townname) {
-
-//         if (_.every(town, check_temp)) {
-//             result.hot.push(townname);
-//         } else if (_.some(town, check_temp)) {
-//             result.warm.push(townname);
-//         }
-
-//     });
-
-//     return result;
-// };
-// module.exports = tempsort;
